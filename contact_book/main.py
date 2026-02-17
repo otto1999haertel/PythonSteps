9c2ed53280b4636d59b12be4660f7649c94ccfd2
ottohartel <otto.haertel@gmail.com>
2026-02-17 20:29:24 +0100

    setting up python challenge

---
+++ contact_book/main.py
@@ -0,0 +1,9 @@
+
+
+
+
+
+#Mini-Kontaktbuch – Kontakte speichern, suchen, bearbeiten, als JSON abspeichern/laden. 
+#Ziel: Dataclasses, JSON-Serialisierung, pathlib, Exception Handling mit eigenen Exceptions.
+if __name__=="__main__":
+    print("Python JSON contact book")
