eebfe777d3c6ad7f6279f26c17e09d83928f5e4e
ottohartel <otto.haertel@gmail.com>
2026-02-20 13:29:44 +0100

    chore: setting up next steps

--- bosten_housing/main.py
+++ bosten_housing/main.py
@@ -1,1 +1,5 @@
 print("Boston Housing Data Set Practise")
+
+#1.⁠ ⁠uv aufsetzen mit ipykernel, pandas, numpy, matplotlib
+#2.⁠ ⁠jupyter notebook aufsetzen
+#3.⁠ ⁠boston housing datensatz einlesen, bissxchen was filtern, visualisieren
